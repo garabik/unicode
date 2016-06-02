a9c17f4d59fa0a14dc3889a680a84c0a2d1a2e91
Radovan Garabík <radovan.garabik-github@kassiopeia.juls.savba.sk>
2016-06-02 15:22:17 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 setup(name='unicode',
-      version='0.2',
+      version='2.3',
       scripts=['unicode', 'paracode'],
       entry_points={'console_scripts': [
           'unicode = unicode:main',
@@ -14,9 +14,9 @@ setup(name='unicode',
       description="Display unicode character properties",
       author="Radovan Garabik",
       author_email='radovan.garabik@kassiopeia.juls.savba.sk',
-      url='https://github.com/garabik/unicode',
+      url='http://kassiopeia.juls.savba.sk/~garabik/software/unicode.html',
       license='GNU GPL v3',
-      keywords=['unicode', 'paracode', 'character'],
+      keywords=['unicode', 'character properties', 'encoding'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
