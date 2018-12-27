2a0fc9abcf48af753e20579f166fc0559b338657
Radovan Garabík <radovan.garabik-github@kassiopeia.juls.savba.sk>
2018-12-27 20:33:38 +0100

    distutil long description

--- setup.py
+++ setup.py
@@ -5,13 +5,21 @@ from setuptools import setup
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
+
+
 setup(name='unicode',
-      version='2.6',
+      version='2.7',
       scripts=['unicode', 'paracode'],
 #      entry_points={'console_scripts': [
 #          'unicode = unicode:main',
 #          'paracode = paracode:main']},
       description="Display unicode character properties",
+      long_description="""
+Enter regular expression, hexadecimal number or some characters as an
+argument. unicode will try to guess what you want to look up.
+Use four-digit hexadecimal number followed by two dots to display
+given unicode block in a nice tabular format..
+"""
       author="Radovan Garabik",
       author_email='radovan.garabik@kassiopeia.juls.savba.sk',
       url='http://kassiopeia.juls.savba.sk/~garabik/software/unicode.html',
